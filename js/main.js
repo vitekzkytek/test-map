d175dcadb0f0220cf55677496403f1fd5adac27b
Tomas Randus <tom.randus@gmail.com>
2014-10-05 20:36:08 +0200

    z

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 
 $(function() {
 
-    var states = Object.keys(data);
-    var metrics = Object.keys(data[states[0]]["metrics"]);
+    var metrics = Object.keys(data.metrics);
+    var states = Object.keys(data.states);
+
     var $menu = $(".menu ul");
     var $svgContainer = $(".svg-container");
+    var $wrapper = $(".wrapper");
+
+    var $popupHolder = $(".popup-holder");
+
+    var $years = $(".bottom-bar .years");
+
+    var $hint = $(".top-hint");
 
     //
     // Open / Hide menu
@@ -15,16 +23,71 @@ $(function() {
     });
 
     //
+    // Refresh popup dialog according it's slectors
+    //
+    var refreshPopup = function(state, metrics) {
+        //
+        for (var i = 1; i < 3; i++) {
+            var $state = $popupHolder.find(".state" + i);
+
+            var state = $popupHolder.find(".state-selector-" + i).val();
+            var year = $popupYearSelector.val();
+            var metricKey = $popupMetricSelector.val();
+            var metric = data.metrics[metricKey];
+
+            $state.find(".score .value").html("x");
+
+            $state.find(".points .value").html(data.states[state].metrics[year][metricKey][0]);
+
+            $state.find(".detail .number").html(data.states[state].metrics[year][metricKey][1] + metric.sign);
+            $state.find(".detail .desc").html(metric.desc);
+        }
+    };
+
+    //
+    // Open detail dialog, the secons state is always Czech
+    //
+    var openPopup = function() {
+        $popupHolder.show();
+        refreshPopup();
+    };
+
+    //
+    // Close popup
+    //
+    var closePopup = function(state, metrics) {
+        $popupHolder.hide();
+        refreshPopup();
+    };
+
+    $popupHolder.find(".btn-close").click(function() {
+        closePopup()
+    });
+    $popupHolder.click(function(e) {
+        var $target = $(e.target);
+
+        if ($target.is(".popup-holder")) {
+            closePopup();
+        }
+    });
+
+    //
     // Selects a particular metrics - repaint map
     //
-    var selectMetric = function(metric) {
+    var selectMetric = function() {
+
+        var year = $popupYearSelector.val();
+        var metricKey = $popupMetricSelector.val();
+        var metric = data.metrics[metricKey];
+        $hint.html(metric.desc);
+
         for (i in states) {
             var state = states[i];
 
             var $state = $(".state-" + state);
             var $statePin = $(".pin-" + state);
 
-            var value = data[state]["metrics"][metric];
+            var value = data.states[state]["metrics"][year][metricKey][0];
             $statePin.find(".number").html(value);
 
             $state.css("fill", "rgba(255, 0, 0, ." + value + ")");
@@ -32,10 +95,72 @@ $(function() {
     };
 
     //
+    // Selects a particular year
+    //
+    var selectYear = function(year) {
+        $popupYearSelector.val(year);
+        $(".year").removeClass("selected");
+        $(".year" + year).addClass("selected");
+    };
+
+
+    //
+    // Copy popup left state elements to right
+    //
+    var $stateI = $popupHolder.find(".state1");
+    $stateI.clone().insertAfter($stateI).removeClass("state1").addClass("state2");
+
+    //
+    // Fill popup states,  metrics, years
+    //
+    var $popupStateSelector1 = $popupHolder.find(".state-selector-1").change(function() {
+        refreshPopup()
+    });
+    var $popupStateSelector2 = $popupHolder.find(".state-selector-2").change(function() {
+        refreshPopup()
+    });
+    var $popupYearSelector = $popupHolder.find(".year-selector").change(function() {
+        refreshPopup()
+    });
+    var $popupMetricSelector = $popupHolder.find(".metric-selector").change(function() {
+        refreshPopup()
+    });
+
+    for (i in states) {
+        var state = states[i];
+
+        $popupStateSelector1.append($("<option>").html(state));
+        $popupStateSelector2.append($("<option>").html(state));
+    }
+
+    $years.addClass("count" + data.years.length);
+    for (i in data.years) {
+        var year = data.years[i];
+        $popupYearSelector.append($("<option>").html(year));
+
+        //years bottom bar
+        $years.prepend($years.find(".year-template").clone().addClass("year" + year).attr("data-year", year).show().html("<span class=\"label\">" + year + "</spen>").removeClass("year-template").click(function() {
+            var $this = $(this);
+
+            selectYear($this.attr("data-year"))
+            selectMetric();
+        }));
+    }
+
+    var metricsKeys = Object.keys(data.metrics);
+
+    for (i in metricsKeys) {
+        var key = metricsKeys[i];
+        //$popupMetricSelector.append($("<option>").html(data.metrics[key][name]).attr("value", key));
+        $popupMetricSelector.append($("<option>").html(data.metrics[key]["name"]).attr("value", key));
+    }
+
+    //
     // Create numbered pins
     //
     for (i in states) {
         var state = states[i];
+
         var $state = $(".state-" + state);
 
         var coordinates = $state[0].getBBox();
@@ -55,27 +180,38 @@ $(function() {
                 + "      <div class=\"comment\">Dom. s připojením k internetu</div>"
                 + "    </div>"
                 + "  </div>"
-                + "  <button class=\"btn btn-detail\">Porovnat</button>"
+                + "  <button data-state=\"" + state + "\" class=\"btn btn-detail\">Porovnat</button>"
                 + "</div>"
                 );
-        $statePin.css("left", 100 / (700 / (coordinates.x + coordinates.width / 2 - data[state]["pinFix"]["x"])) + "%");
-        $statePin.css("bottom", 100 - (100 / ((700 * 0.79) / (coordinates.y + coordinates.height / 2 - data[state]["pinFix"]["y"]))) + "%");
-        $svgContainer.append($statePin);
+        $statePin.css("left", 100 / (700 / (coordinates.x + coordinates.width / 2 - data.states[state]["pinFix"]["x"])) + "%");
+        $statePin.css("bottom", 100 - (100 / ((700 * 0.79) / (coordinates.y + coordinates.height / 2 - data.states[state]["pinFix"]["y"]))) + "%");
+        //$svgContainer.append($statePin);
+        $wrapper.append($statePin);
+        $statePin.find(".btn-detail").click(function() {
+            $popupStateSelector1.val($(this).attr("data-state"));
+            $popupStateSelector2.val(data.mainState);
+            openPopup();
+        });
     }
 
     for (i in metrics) {
         var metric = metrics[i];
 
-        var $menuItem = $("<li>").html("<span data-metric=\"" + metric + "\">Metrika '" + metric + "'</span>").click(function(e) {
-            selectMetric(e.target.getAttribute("data-metric"));
+        var $menuItem = $("<li>").html("<span data-metric=\"" + metric + "\">" + data.metrics[metric].name + "</span>").click(function(e) {
+            $popupMetricSelector.val(e.target.getAttribute("data-metric"));
+            selectMetric();
         });
 
         $menu.append($menuItem);
     }
 
     //select first metrics
-    selectMetric(Object.keys(data[states[0]]["metrics"])[0]);
+    selectMetric();
+    selectYear($popupYearSelector.val());
 
+    //open dialog 'czech', 'a'
+//    openPopup();
+    refreshPopup();
 
 //    for (i in states) {
 //        var state = states[i];
